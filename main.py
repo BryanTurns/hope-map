0531dc34f067d0b3eb6e747ad90822f65bb4074c
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2025-03-11 19:25:27 -0600

    Can now get 1991 cols to match with the most common from 2011

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ df_combined = df_combined.set_index("AREA_NAME")
 cols_p2011 = df_combined.columns.str.contains("_P_2011")
 cols_p1991 = df_combined.columns.str.contains("_P_1991")
 df_p2011 = df_combined.loc[:, cols_p2011]
-df_p1991 = df_combined.loc[:, cols_p2011]
+df_p1991 = df_combined.loc[:, cols_p1991]
 
-assam = df_combined.loc["ASSAM"]
-print(assam)
+assam_2011 = df_p2011.loc["ASSAM"]
+assam_2011 = assam_2011.sort_values(ascending=False).head(5)
+# Create a Regex to allow filtering by languages
+pattern = "^("
+for language, proportion in assam_2011.items():
+    pattern += language.split("_")[0] + "|"
+pattern = pattern[:-1]
+pattern += ").*"
+selected_cols_1991 = df_p1991.columns.str.contains(pattern, case=False, regex=True)
+assam_1991 = df_p1991.loc["ASSAM", selected_cols_1991]
+
+
+fig, ax = plt.subplots(layout='constrained')
+for language, proportion in assam_2011.items():
+    print(language, proportion)
 # Convert cols into key
 
 # plt.bar(x=df_c)
